ab9b2ce78694ed4525fe1d4dcf7079ef6fda89ac
Sophie Machin <sophie.machin@thislabs.org>
2024-01-23 14:51:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,28 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="thiscovery-lib",  # Replace with your own username
+    name="thiscovery-lib-acuity",  # Replace with your own username
     version="2021.2.3.2",
     author="Thiscovery team",
     author_email="support@thiscovery.org",
-    description="Thiscovery library",
+    description="Thiscovery library for Acuity",
     install_requires=[
-        "boto3",
-        "botocore",
-        "python-dateutil",
-        "jsons",
-        "python-json-logger",
-        "requests",
-        # requests==2.28.2
-        "simplejson",
-        "validators",
-        "sendgrid",
+
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/THIS-Labs/thiscovery-lib",
+    url="https://github.com/THIS-Labs/thiscovery-lib-acuity",
     package_data={
-        "thiscovery_lib": ["countries.json"],
+        "thiscovery_lib": [],
     },
     packages=setuptools.find_packages(),
     classifiers=[
@@ -33,5 +24,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
